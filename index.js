095ca37d5cb48c7ae18e83d4f8d57351d90196a0
Kyle W-M <25kylworc@gmail.com>
2020-11-21 11:58:20 -0800

    fixed parcel scoping problem

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function scrollHandler() {
 }
 
 
-function openModal(){
-    console.log("Met")
+window.openModal = (modal_id)=>{
+    console.log("modal_id",modal_id)
     var modalEl = document.createElement('div');
-    modalEl.innerHTML = '<p>The world\'s simplest modal is showing!</p><button class="close-btn">Close Modal</button>';
+    modalEl.innerHTML = '<p>The world\'s simplest modal is showing!</p><button class="close-btn" onclick="closeModal()">Close Modal</button>';
     var closeButton = modalEl.getElementsByClassName('close-btn')[0];
     var showButton = document.getElementsByClassName('surface-image')[0];
 
@@ -52,6 +52,5 @@ function closeModal(modal){
 // Bind events to window
 // window.onresize = resizeHandler;
 window.onscroll = scrollHandler;
-window.onclick = openModal;
 console.log(document.getElementsByClassName('surface-image'))
 // window.addEventListener("mousemove", mouseMoveHandler);
